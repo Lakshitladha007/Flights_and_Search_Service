9fed2ab69ec4b83b40733699a9a74254b896405d
Lakshit Ladha <lakshitladha05@gmail.com>
2024-01-19 03:31:11 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,27 +1,62 @@
-const { City }=require('../models/index');
+//repository layer is responsible for commuicationg with any data source such as Database or anything else.
 
+const { City }=require('../models/index');     //In order to assure city repository and all works fine, we need to have access to the models. So we are requiring models. 
+                                               // We will add the path of 'index' file as it will give access to all the models 
+                                               //present in the folder, as "index" file contains logic due to which all models 
+                                               //can be accessed form it
 class CityRepository{
+
     async createCity({name}){
         try {
-            const city=await City.create({name});
+            const city=await City.create({
+                name
+            });
             return city;
         } catch (error) {
+            console.log("Something went wrong in respository layer");
             throw {error};
         }
     }
 
     async deleteCity(cityId){
         try {
-            const response=await City.destroy({
+            await City.destroy({
                 where:{
                     id:cityId,
                 }
             });
-            return response;
+            return true;
         } catch (error) {
+            console.log("Something went wrong in respository layer");
             throw {error};
         }
     }
+
+    async updateCity(data,cityId){
+        try {
+            const city=await City.update(data,{
+                where:{
+                    id:cityId,
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in respository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city=await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in respository layer");
+            throw {error};
+        }
+    }
+
+  
 }
 
 module.exports=CityRepository;
