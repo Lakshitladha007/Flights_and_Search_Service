cf2e0af628133aa32d6f3baa31c1b7695c43f08f
Lakshit Ladha <lakshitladha05@gmail.com>
2024-03-21 04:46:25 +0530

    Added error code enums

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,11 +1,12 @@
 const { FlightService }=require("../services/index");
+const { SuccessCodes }= require("../utils/error-codes");
 
 const flightService=new FlightService();
 
 const create= async (req, res)=>{
     try {
         const flight=await flightService.createFlight(req.body);
-        return res.status(201).json({
+        return res.status(SuccessCodes.CREATED).json({
             data: flight,
             success:true,
             message:"Successfully created a flight",
@@ -25,7 +26,7 @@ const create= async (req, res)=>{
 const getAll= async(req,res)=>{
     try {
         const response=await flightService.getAllFlightData(req.query);
-        return res.status(200).json({
+        return res.status(SuccessCodes.OK).json({
             data: response,
             success:true,
             message:"Successfully fetched the flights",
--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -2,6 +2,7 @@
 // abide the API contracts. Middlewares are functions which have access to request object and response object, 
 // and the next middleware.
 
+const { ClientErrorCodes }=require("../utils/error-codes");
 const validateCreateFlight= (req, res, next) => { // If any of the property which is mandatory for creating a 
                                                   // flight is not present than techinally we should not allow the
                                                   // request to go further because eventually it is going to fail
@@ -13,7 +14,7 @@ const validateCreateFlight= (req, res, next) => { // If any of the property whic
         !req.body.arrivalTime ||
         !req.body.price
     ){  // if any of the body params is missing we come inside the if check
-        return res.status(400).json({ // '400' is for bad request i.e. our request body does not follows API 
+        return res.status(ClientErrorCodes.BAD_REQUEST).json({ // '400' is for bad request i.e. our request body does not follows API 
                                       // contract
             data:{},
             success: false,
