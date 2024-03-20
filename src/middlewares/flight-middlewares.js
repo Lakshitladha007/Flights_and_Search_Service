3bf618644d4a0efcac97b4c856311ec7b33c0256
Lakshit Ladha <lakshitladha05@gmail.com>
2024-03-21 04:27:35 +0530

    Added flight-create request middleware

---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,30 @@
+// Middlewares are just simple functions that help us to filter out the requests from users which does not 
+// abide the API contracts. Middlewares are functions which have access to request object and response object, 
+// and the next middleware.
+
+const validateCreateFlight= (req, res, next) => { // If any of the property which is mandatory for creating a 
+                                                  // flight is not present than techinally we should not allow the
+                                                  // request to go further because eventually it is going to fail
+    if( !req.body.flightNumber ||
+        !req.body.airplaneId  ||
+        !req.body.departureAirportId ||
+        !req.body.arrivalAirportId ||
+        !req.body.departureTime ||
+        !req.body.arrivalTime ||
+        !req.body.price
+    ){  // if any of the body params is missing we come inside the if check
+        return res.status(400).json({ // '400' is for bad request i.e. our request body does not follows API 
+                                      // contract
+            data:{},
+            success: false,
+            message: "Invalid request body for create flight",
+            err: "Missing mandatory properties to create a flight"
+        })
+    }
+
+    next(); // If above if check is not executed we will just call the next middleware
+}
+
+module.exports={
+    validateCreateFlight
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const CityController=require("../../controllers/city-controller");
 const FlightController=require("../../controllers/flight-controller");
 const AirportController= require("../../controllers/airport-crud-controller");
+const { FlightMiddlewares } =require("../../middlewares/index");
 
 const router=express.Router();
 
@@ -19,7 +20,10 @@ router.get('/city',CityController.getAll);
 router.patch('/city/:id', CityController.update);
 
 
-router.post('/flights',FlightController.create);
+router.post(
+    '/flights',
+    FlightMiddlewares.validateCreateFlight,
+    FlightController.create);
 router.get('/flights',FlightController.getAll);
 
 router.post('/airports', AirportController.create);
